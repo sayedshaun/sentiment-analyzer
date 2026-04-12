d235e8a98926846dceb847d758ee688c3b64540b
sayedshaun <sayedshaun4@gmail.com>
2026-04-12 22:30:01 +0600

    initial commit

---
+++ services/ml/stt/infer.py
@@ -0,0 +1,42 @@
+import litserve as ls
+import nemo.collections.asr as nemo_asr
+import tempfile
+import os
+import asyncio
+from utils import convert_to_wav, chunk_audio
+from config import settings
+
+
+class ASRLitAPI(ls.LitAPI):
+
+    def setup(self, device):
+        self.model = nemo_asr.models.ASRModel.from_pretrained(settings.TTS_MODEL)
+
+    def transcribe(self, path: str) -> str:
+
+        wav_path = convert_to_wav(path)
+        chunks = chunk_audio(wav_path, chunk_length_ms=30_000)
+
+        transcription = []
+        for chunk_path in chunks:
+            result = self.model.transcribe([chunk_path])
+            transcription.append(result[0].text)
+
+            os.remove(chunk_path)
+        os.remove(wav_path)
+        return " ".join(transcription)
+
+    async def predict(self, request):
+        audio_file = request["file"]
+        suffix = os.path.splitext(audio_file.filename)[1]
+        tmp_path = tempfile.NamedTemporaryFile(delete=False, suffix=suffix).name
+
+        with open(tmp_path, "wb") as f:
+            f.write(await audio_file.read())
+
+        text = await asyncio.to_thread(self.transcribe, tmp_path)
+        os.remove(tmp_path)
+        return {"text": text}
+
+    def encode_response(self, output):
+        return output
